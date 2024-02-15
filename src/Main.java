5fbda339cdfe02dc76c971f74f33904e0c087387
sulfurmagic <johnaul81@gmail.com>
2024-02-15 21:47:52 +0300

    conditionals1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        int amount = 3450; // сумма пополнения
+        int balance1 = 100; // начальный баланс
+
+        int bonus;
+        if (amount > 1000) {
+            bonus = amount / 100;
+        } else {
+            bonus = 0;
+        }
+
+        int balance2 = balance1 + bonus + amount; // итоговый баланс
+
+        System.out.println("Бонусов начислено: " + bonus);
+        System.out.println("Сейчас на балансе: " + balance2);
 
 
 
